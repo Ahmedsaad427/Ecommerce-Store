3dc0d4588a70ed4aea8533a152697d417c2e5ab8
Compulap <Compulap@AHMED-SAAD>
2025-04-18 17:18:41 +0200

    ValidationErrorHandling

--- Ecommerce-Store.API/Program.cs
+++ Ecommerce-Store.API/Program.cs
@@ -1,11 +1,13 @@
 using System.Reflection.Metadata;
 using Domain.Contracts;
 using Ecommerce_Store.API.Middlewares;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Services;
 using Services.Abstractions;
-
+using Shared.ErrorModels;
 using AssemblyMapping = Services.AssemblyReference;
 public class Program
 {
@@ -29,6 +31,29 @@ public class Program
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
         builder.Services.AddAutoMapper(typeof(AssemblyMapping).Assembly);
         builder.Services.AddScoped<IServiceManager, ServiceManager>();
+
+        builder.Services.Configure<ApiBehaviorOptions>(config =>
+        {
+            config.InvalidModelStateResponseFactory = actionContext =>
+            {
+                var errors = actionContext.ModelState
+                    .Where(m => m.Value.Errors.Any())
+                    .Select(m => new ValidationError
+                    {
+                        Field = m.Key,
+                        Errors = m.Value.Errors.Select(e => e.ErrorMessage).ToList()
+                    });
+
+                var response = new ValidationErrorReponse()
+                {
+                    Errors = errors
+                };
+
+                return new BadRequestObjectResult(response);
+            };
+        });
+
+
         var app = builder.Build();
         app.UseMiddleware<GlobalErrorHandlingMiddleware>(); // Register the global error handling middleware
         #region Seeding
---
+++ Shared/ErrorModels/ValidationErrorReponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Shared.ErrorModels
+{
+    public class ValidationErrorReponse
+    {
+        public int statusCode { get; set; } = StatusCodes.Status400BadRequest;
+
+        public string ErrorMessage { get; set; } = "Validation Error";
+
+        public IEnumerable<ValidationError> Errors { get; set; }
+
+    }
+}
